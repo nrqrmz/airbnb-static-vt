b38f296b6deb4b32611a6d1fed71459c1b74c792
Enrique Ramirez <nrqrmz@gmail.com>
2022-10-19 14:50:27 -0500

    add test enviroment

---
+++ test/system/flats_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class FlatsTest < ApplicationSystemTestCase
+  test 'visiting flats page' do
+    visit '/'
+
+    assert_selector 'h1', text: 'Flats'
+  end
+end
