34872152e681a5cbdcb994462c79814c441b7b21
Enrique Ramirez <nrqrmz@gmail.com>
2022-10-19 15:29:34 -0500

    add show and update index content

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   root to: 'flats#index'
+  get 'flats/:id', to: 'flats#show', as: :flat
 end
